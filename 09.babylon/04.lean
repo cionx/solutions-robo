b400184b89cb30a06df04ec34599f0f15a9c3517
Jendrik Stelzner <jendrikstelzner@web.de>
2025-08-17 14:24:00 +0200

    Add missing indentation.

--- 09.babylon/04.lean
+++ 09.babylon/04.lean
@@ -2,19 +2,19 @@ symm
 apply sum_subset
 · rw [subset_iff]
   simp
-intro x hx1
-intro hx2
-simp at hx1
-simp at hx2
-have hx3 : x < 3
-· omega
-have h : x = 0 ∨ x = 1 ∨ x = 2
-· omega
--- There should be a better way to do the following.
-obtain h | h | h := h
-· rw [h]
-  simp
-· rw [h]
-  simp
-· rw [h]
-  simp
+· intro x hx1
+  intro hx2
+  simp at hx1
+  simp at hx2
+  have hx3 : x < 3
+  · omega
+  have h : x = 0 ∨ x = 1 ∨ x = 2
+  · omega
+  -- There should be a better way to do the following.
+  obtain h | h | h := h
+  · rw [h]
+    simp
+  · rw [h]
+    simp
+  · rw [h]
+    simp
